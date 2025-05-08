ac550f2b66111c8f97fbaed7ff657009adbe3e43
KIM Hyeongi <89232601+BodleHG@users.noreply.github.com>
2025-05-08 16:42:19 +0900

    Control Plane - grouping algorithm add

---
+++ control-plane/grouping-algo/main.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+
+def run_cmd(cmd):
+    try:
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True, timeout=5)
+        if result.returncode == 0:
+            print(f"\n$ {cmd}\n{result.stdout}")
+        else:
+            print(f"\n$ {cmd}\nError: {result.stderr}")
+    except Exception as e:
+        print(f"Exception while running '{cmd}': {e}")
+
+while True:
+    run_cmd("kubectl top nodes")
+    run_cmd("kubectl top pods --all-namespaces")
+    run_cmd("kubectl get crd")
+    time.sleep(10)
